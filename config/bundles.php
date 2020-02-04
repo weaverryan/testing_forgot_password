f4324e0497706cd5ce2322f9cbe01219172f128a
Jesse Rushlow <jr@geeshoe.com>
2020-02-04 06:40:56 -0500

    added database test fixtures

--- config/bundles.php
+++ config/bundles.php
@@ -7,5 +7,6 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
-    SymfonyCasts\Bundle\ResetPassword\SymfonyCastsPasswordResetBundle::class => ['all' => true ]
+    SymfonyCasts\Bundle\ResetPassword\SymfonyCastsPasswordResetBundle::class => ['all' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
