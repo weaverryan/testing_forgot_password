cc7a2c13a532b1c8767ed3a2fbd14733a9e93b5c
Jesse Rushlow <jr@geeshoe.com>
2020-03-09 11:37:40 -0400

    refactored request template

--- src/Resource/templates/twig_request.tpl.php
+++ src/Resource/templates/twig_request.tpl.php
@@ -1,15 +1,23 @@
 {% extends 'base.html.twig' %}
 
-{% block title %}Recover your password{% endblock %}
+{% block title %}Reset your password{% endblock %}
 
 {% block body %}
 {% for message in app.flashes('reset_password_error') %}
     <section class="flash-error"><p>{{ message }}</p></section>
 {% endfor %}
-<h1>Recover your password</h1>
+    <h1>Reset your password</h1>
 
 {{ form_start(requestForm) }}
 {{ form_row(requestForm.<?= $email_field ?>) }}
-    <button class="btn btn-primary">Send e-mail</button>
+    <small>
+        Enter your email address and we we will send you a
+        link to reset your password.
+    </small>
+
+    {{ form_start(requestForm) }}
+    {{ form_row(requestForm.email) }}
+
+    <button class="btn btn-primary">Send password reset email</button>
 {{ form_end(requestForm) }}
 {% endblock %}
