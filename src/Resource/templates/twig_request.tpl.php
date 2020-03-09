d770675b711846919b31a488bbfbae5d452e9132
Jesse Rushlow <jr@geeshoe.com>
2020-03-09 11:14:03 -0400

    fixed missing identation in twig request and reset templates

--- src/Resource/templates/twig_request.tpl.php
+++ src/Resource/templates/twig_request.tpl.php
@@ -4,13 +4,12 @@
 
 {% block body %}
 {% for message in app.flashes('reset_password_error') %}
-<section class="flash-error"><p>{{ message }}</p></section>
+    <section class="flash-error"><p>{{ message }}</p></section>
 {% endfor %}
 <h1>Recover your password</h1>
 
 {{ form_start(requestForm) }}
 {{ form_row(requestForm.<?= $email_field ?>) }}
-
-<button class="btn btn-primary">Send e-mail</button>
+    <button class="btn btn-primary">Send e-mail</button>
 {{ form_end(requestForm) }}
 {% endblock %}
