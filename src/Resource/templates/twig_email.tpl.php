cc6c508aa4417cc9791d303b1ce8652ce3b18f06
Jesse Rushlow <jr@geeshoe.com>
2020-03-10 05:50:39 -0400

    fixed var reference

--- src/Resource/templates/twig_email.tpl.php
+++ src/Resource/templates/twig_email.tpl.php
@@ -2,7 +2,7 @@
 
 <p>
     To reset your password, please visit
-    <a href="{{ url('app_reset_password', {tokenAndSelector: resetToken.token}) }}">here</a>
+    <a href="{{ url('app_reset_password', {token: resetToken.token}) }}">here</a>
     This link will expire in {{ resetToken.ExpiresAt|date('Y-m-d') }}TODO - convert to hours! hours.
 </p>
 
